fc3dc0b0fb5d2eb9d47a3f3093c2389a61eb7e65
harshvardhangupta <harshvardhan_gupta@yahoo.com>
2018-08-01 17:46:53 +0530

    Converted ResendActivationForm to functional

--- src/ResendActivationPage/ResendActivationForm/ResendActivationForm.js
+++ src/ResendActivationPage/ResendActivationForm/ResendActivationForm.js
@@ -14,86 +14,84 @@ const schema = yup.object().shape({
 });
 
 
-class ResendActivationForm extends React.Component {
+function ResendActivationForm() {
 
-    render() {
-        return (<Formik
+    return (<Formik
 
-            initialValues={{email: '', recaptcha: '', info: ''}}
+        initialValues={{email: '', recaptcha: '', info: ''}}
 
-            validationSchema={schema}
+        validationSchema={schema}
 
-            onSubmit={(values, {setSubmitting, setErrors}) => {
+        onSubmit={(values, {setSubmitting, setErrors}) => {
 
-                ResendActivationMail(values.email, values.recaptcha).then(() => {
-                    toast.success("Email Successfully Resent!");
-                    history.push("/login");
+            ResendActivationMail(values.email, values.recaptcha).then(() => {
+                toast.success("Email Successfully Resent!");
+                history.push("/login");
 
-                }).catch(({response}) => {
-                    setErrors({info: response.data.message});
-                    setSubmitting(false);
-                });
-
-
-            }}
-
-            render={({values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting, setFieldValue}) => (
-
-                <form onSubmit={handleSubmit}>
-
-                    <div className="col-12 color--error">{errors.info}</div>
+            }).catch(({response}) => {
+                setErrors({info: response.data.message});
+                setSubmitting(false);
+            });
 
 
-                    <div className="row">
+        }}
 
-                        <div className="col-8 ">
+        render={({values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting, setFieldValue}) => (
 
-                            <input
-                                type="email"
-                                name="email"
-                                placeholder="Enter your email"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.email}
-                            />
-                        </div>
+            <form onSubmit={handleSubmit}>
+
+                <div className="col-12 color--error">{errors.info}</div>
 
 
-                        <button className="btn btn--primary col-4 " type="submit" disabled={isSubmitting}>
-                            Resend Activation Email
-                        </button>
+                <div className="row">
 
-                        {touched.email && errors.email && <div className="col-12 color--error">{errors.email}</div>}
+                    <div className="col-8 ">
 
-                    </div>
-
-
-                    <div className={"col-8"}>
-
-                        <Recaptcha
-                            sitekey="6LfJGVMUAAAAAJJtME41Fh4D_sQUAcIJSKqSLwAN"
-                            render="explicit"
-                            theme="light"
-
-                            verifyCallback={(response) => {
-                                setFieldValue("recaptcha", response);
-
-                            }}
-                            onloadCallback={() => {
-
-                            }}
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Enter your email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
                         />
-                        {touched.recaptcha && errors.recaptcha && <div className="col-4 color--error">{errors.recaptcha}</div>}
-
                     </div>
 
 
-                </form>
+                    <button className="btn btn--primary col-4 " type="submit" disabled={isSubmitting}>
+                        Resend Activation Email
+                    </button>
 
-            )}
-        />);
-    };
-}
+                    {touched.email && errors.email && <div className="col-12 color--error">{errors.email}</div>}
+
+                </div>
+
+
+                <div className={"col-8"}>
+
+                    <Recaptcha
+                        sitekey="6LfJGVMUAAAAAJJtME41Fh4D_sQUAcIJSKqSLwAN"
+                        render="explicit"
+                        theme="light"
+
+                        verifyCallback={(response) => {
+                            setFieldValue("recaptcha", response);
+
+                        }}
+                        onloadCallback={() => {
+
+                        }}
+                    />
+                    {touched.recaptcha && errors.recaptcha && <div className="col-4 color--error">{errors.recaptcha}</div>}
+
+                </div>
+
+
+            </form>
+
+        )}
+    />)
+};
 
 
 export default ResendActivationForm;
