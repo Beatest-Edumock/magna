12c42e91ffe179419a47ce459fc6eae40d9d647c
harshvardhangupta <harshvardhan_gupta@yahoo.com>
2018-09-02 20:57:32 -0400

    Fixed HomePage Imports and sticky top.

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,15 +1,12 @@
 import React from "react";
 import Typed from 'react-typed';
-import Recaptcha from 'react-recaptcha';
-import config from 'config';
-import {NavBarWithButtonsContainer} from "../Layout/NavBarWithButtons/NavBarWithButtonsContainer";
+import {NavBarWithButtonsContainer} from "../Layout/NavBar/NavBarWithButtons/NavBarWithButtonsContainer";
 import 'react-toastify/dist/ReactToastify.css';
-import {Button, Container, Jumbotron, Form, FormGroup, Label, Input, FormText, Row, Col} from 'reactstrap';
+import {Button, Col, Container, Jumbotron, Row} from 'reactstrap';
 import {FeatureCard} from "../Common/FeatureCard/FeatureCard";
 import {LargeFeatureCard} from "../Common/LargeFeatureCard/LargeFeatureCard";
 import Flickity from 'react-flickity-component';
-import {NavLink} from 'react-router-dom';
-import {FEATURE_CARD_ELEMENTS,LARGE_FEATURE_CARD_ELEMENTS,COLLEGE_IMAGES,TESTIMONIALS,RECOGNITIONS} from './data'
+import {COLLEGE_IMAGES, FEATURE_CARD_ELEMENTS, LARGE_FEATURE_CARD_ELEMENTS, RECOGNITIONS, TESTIMONIALS} from './data'
 import {ContactUsContainer} from '../ContactUs/ContactUsForm/ContactUsContainer'
 import {Footer} from '../Layout/Footer/Footer'
 
@@ -34,7 +31,6 @@ function typedString() {
 }
 
 
-
 const bodyStyle = {
     background: 'radial-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9)) ,url(/img/landing-1.jpg) no-repeat center',
     backgroundSize: "100% auto",
@@ -47,16 +43,13 @@ const flickityOptions = {
 };
 
 
-
 function HomePage() {
     return (
 
         <div>
-            <div className="container-fluid">
 
-                <NavBarWithButtonsContainer/>
+            <NavBarWithButtonsContainer/>
 
-            </div>
 
             <div>
 
@@ -84,7 +77,7 @@ function HomePage() {
                         {
                             FEATURE_CARD_ELEMENTS.map((feature_card) => {
                                 return (
-                                    <FeatureCard 
+                                    <FeatureCard
                                         icon={feature_card.icon}
                                         text={feature_card.text}
                                     />
@@ -105,7 +98,7 @@ function HomePage() {
                             {
                                 LARGE_FEATURE_CARD_ELEMENTS.map((feature_card) => {
                                     return (
-                                        <LargeFeatureCard 
+                                        <LargeFeatureCard
                                             icon={feature_card.icon}
                                             text={feature_card.text}
                                         />
@@ -118,40 +111,46 @@ function HomePage() {
                 </Container>
 
             </div>
-            <div style={{paddingTop: '60px',paddingBottom: '60px'}}>
-                <span style={{fontFamily: 'Open Sans,Helvetica,Arial,sans-serif',textAlign: 'center',display: 'block', paddingBottom: '32px',fontSize:'32px'}}>STUDENT PARTNERS</span>
+            <div style={{paddingTop: '60px', paddingBottom: '60px'}}>
+                <span style={{
+                    fontFamily: 'Open Sans,Helvetica,Arial,sans-serif',
+                    textAlign: 'center',
+                    display: 'block',
+                    paddingBottom: '32px',
+                    fontSize: '32px'
+                }}>STUDENT PARTNERS</span>
                 <Flickity
-                  className={'carousel'} // default ''
-                  elementType={'div'} // default 'div'
-                  options={flickityOptions} // takes flickity options {}
-                  disableImagesLoaded={false} // default false
-                  reloadOnUpdate // default false
+                    className={'carousel'} // default ''
+                    elementType={'div'} // default 'div'
+                    options={flickityOptions} // takes flickity options {}
+                    disableImagesLoaded={false} // default false
+                    reloadOnUpdate // default false
                 >
                     {
                         COLLEGE_IMAGES.map((image) => {
                             return (
-                                <img src={image.img} style={{width: 180,marginRight: 200}}/>
+                                <img src={image.img} style={{width: 180, marginRight: 200}}/>
 
                             );
                         })
                     }
                 </Flickity>
             </div>
-            <div style={{paddingTop: '60px',paddingBottom: '60px',backgroundColor: 'black',color:'white'}}>
+            <div style={{paddingTop: '60px', paddingBottom: '60px', backgroundColor: 'black', color: 'white'}}>
                 <Flickity
-                  className={'carousel'} // default ''
-                  elementType={'div'} // default 'div'
-                  options={flickityOptions} // takes flickity options {}
-                  disableImagesLoaded={false} // default false
-                  reloadOnUpdate // default false
+                    className={'carousel'} // default ''
+                    elementType={'div'} // default 'div'
+                    options={flickityOptions} // takes flickity options {}
+                    disableImagesLoaded={false} // default false
+                    reloadOnUpdate // default false
                 >
                     {
                         TESTIMONIALS.map((testimonial) => {
                             return (
-                                <div style={{display: 'flex',flexWrap: 'wrap',marginRight:200,width:'80vw'}}>
-                                    <img src={testimonial.img} style={{height:100,marginRight: 60}}/>
-                                    <div style={{display: 'flex',flexDirection: 'column'}}>
-                                        <span className="h3" style={{width:'50vw'}}>{testimonial.message}</span>
+                                <div style={{display: 'flex', flexWrap: 'wrap', marginRight: 200, width: '80vw'}}>
+                                    <img src={testimonial.img} style={{height: 100, marginRight: 60}}/>
+                                    <div style={{display: 'flex', flexDirection: 'column'}}>
+                                        <span className="h3" style={{width: '50vw'}}>{testimonial.message}</span>
                                         <h5>{testimonial.name}</h5>
                                         <span>{testimonial.place}</span>
                                     </div>
@@ -162,19 +161,20 @@ function HomePage() {
                 </Flickity>
             </div>
 
-            <div style={{paddingTop: '60px',paddingBottom: '60px'}}>
-                <span style={{fontFamily: 'Open Sans,Helvetica,Arial,sans-serif',textAlign: 'center',display: 'block', paddingBottom: '32px',fontSize:'32px'}}>RECOGNITIONS</span>
+            <div style={{paddingTop: '60px', paddingBottom: '60px'}}>
+                <span
+                    style={{fontFamily: 'Open Sans,Helvetica,Arial,sans-serif', textAlign: 'center', display: 'block', paddingBottom: '32px', fontSize: '32px'}}>RECOGNITIONS</span>
                 <Flickity
-                  className={'carousel'} // default ''
-                  elementType={'div'} // default 'div'
-                  options={flickityOptions} // takes flickity options {}
-                  disableImagesLoaded={false} // default false
-                  reloadOnUpdate // default false
+                    className={'carousel'} // default ''
+                    elementType={'div'} // default 'div'
+                    options={flickityOptions} // takes flickity options {}
+                    disableImagesLoaded={false} // default false
+                    reloadOnUpdate // default false
                 >
                     {
                         RECOGNITIONS.map((image) => {
                             return (
-                                <img src={image.img} style={{width: 180,marginRight: 200}}/>
+                                <img src={image.img} style={{width: 180, marginRight: 200}}/>
 
                             );
                         })
@@ -182,20 +182,23 @@ function HomePage() {
                 </Flickity>
             </div>
             <Container style={{paddingTop: 32}}>
-                <span style={{fontFamily: 'Open Sans,Helvetica,Arial,sans-serif',textAlign: 'center',display: 'block',fontSize:'32px',paddingBottom: 16}}>CONTACT US</span>
+                <span style={{fontFamily: 'Open Sans,Helvetica,Arial,sans-serif', textAlign: 'center', display: 'block', fontSize: '32px', paddingBottom: 16}}>CONTACT US</span>
                 <Row>
                     <Col style={{paddingRight: '6%'}}>
-                        <p><span style={{fontSize: 20,color:'gray',display: 'inline'}}>Email:</span><a href="mailto:hello@beatest.in"><span style={{fontSize: 20,display: 'inline',textDecoration: 'underline'}}>hello@beatest.in</span></a></p>
-                        <p style={{fontSize: 24,color:'gray'}}><span>#7th Floor, Monibhandar Building Webel Bhavan Premises, Sector 5, Ring Road, Bidhannagar, West Bengal-700091, India</span></p>
-                        <p style={{fontSize: 14,color:'#666'}}><span>Drop us an email or visit us anytime, we endeavour to answer all enquiries within 24 hours on business days.</span></p>
+                        <p><span style={{fontSize: 20, color: 'gray', display: 'inline'}}>Email:</span><a href="mailto:hello@beatest.in"><span
+                            style={{fontSize: 20, display: 'inline', textDecoration: 'underline'}}>hello@beatest.in</span></a></p>
+                        <p style={{fontSize: 24, color: 'gray'}}><span>#7th Floor, Monibhandar Building Webel Bhavan Premises, Sector 5, Ring Road, Bidhannagar, West Bengal-700091, India</span>
+                        </p>
+                        <p style={{fontSize: 14, color: '#666'}}>
+                            <span>Drop us an email or visit us anytime, we endeavour to answer all enquiries within 24 hours on business days.</span></p>
                     </Col>
                     <Col>
-                        <ContactUsContainer />
+                        <ContactUsContainer/>
                     </Col>
                 </Row>
 
             </Container>
-            <Footer />
+            <Footer/>
         </div>
     )
 }
