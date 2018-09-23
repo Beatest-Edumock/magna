82e5e14f77cf46866486de1cf31d89f1b7b839dd
harshvardhangupta <harshvardhan_gupta@yahoo.com>
2018-09-23 02:00:58 -0400

    Added background color to choices

--- src/test-framework/ExamPage/components/QuestionContent/Workshop/MCQ/MCQUI.js
+++ src/test-framework/ExamPage/components/QuestionContent/Workshop/MCQ/MCQUI.js
@@ -3,31 +3,36 @@ import React, {Component} from 'react';
 function MCQUI(props) {
 
 
-    return(
+    return (
 
         <div>
             <div dangerouslySetInnerHTML={{__html: props.questionHtml}}/>
-        <div className='form-check'>
-            {
-            props.choices.map((choice) => {
-                const shouldChecked = (choice.id === props.currentChoice);
+            <div className='form-check'>
+                {
+                    props.choices.map((choice) => {
+                        const shouldChecked = (choice.id === props.currentChoice);
 
-                console.log(choice.id + " : " + props.currentChoice + " -> " +shouldChecked);
-                return (
-                    <div className="card">
-                <div className="card-body mx-2">
-                <input className="form-check-input" name="choiceRadio" type="radio"
-                       onChange={()=>props.mcqCallback(choice.id)}
-                       id={choice.id} checked={shouldChecked}/>
-                <label className="form-check-label"><div dangerouslySetInnerHTML={{__html: choice.html}}/></label>
+                        console.log(choice.id + " : " + props.currentChoice + " -> " + shouldChecked);
+                        return (
+                            <div className="card rounded rounded-0 bg-light">
+                                <div className="card-body mx-2 align-items-center d-flex">
 
-                </div> </div>
-            )
-        })
-            }
+                                    <input className="form-check-input " name="choiceRadio" type="radio"
+                                           onChange={() => props.mcqCallback(choice.id)}
+                                           id={choice.id} checked={shouldChecked}/>
+                                    <label className="form-check-label">
+                                        <div dangerouslySetInnerHTML={{__html: choice.html}}/>
+                                    </label>
 
-        </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
             </div>
-        )
+        </div>
+    )
 }
+
 export {MCQUI}
