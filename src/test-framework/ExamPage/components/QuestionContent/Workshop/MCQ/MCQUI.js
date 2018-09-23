7d67e720b89c686afcc74afb2fc38ce988b9dd63
harshvardhangupta <harshvardhan_gupta@yahoo.com>
2018-09-23 19:58:44 -0400

    Fixed out of place choices panel

--- src/test-framework/ExamPage/components/QuestionContent/Workshop/MCQ/MCQUI.js
+++ src/test-framework/ExamPage/components/QuestionContent/Workshop/MCQ/MCQUI.js
@@ -7,7 +7,7 @@ function MCQUI(props) {
 
         <div className="">
             <div dangerouslySetInnerHTML={{__html: props.questionHtml}}/>
-            <div className='form-check'>
+            <div className=''>
                 {
                     props.choices.map((choice) => {
                         const shouldChecked = (choice.id === props.currentChoice);
@@ -15,7 +15,7 @@ function MCQUI(props) {
                         console.log(choice.id + " : " + props.currentChoice + " -> " + shouldChecked);
                         return (
                             <div className="card rounded rounded-0 bg-light">
-                                <div className="card-body mx-2 align-items-center d-flex">
+                                <div className="card-body mx-2 align-items-center d-flex d-inline">
 
                                     <input className="form-check-input " name="choiceRadio" type="radio"
                                            onChange={() => props.mcqCallback(choice.id)}
