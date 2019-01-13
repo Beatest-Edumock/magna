a8372e3080601e25e91345fdd925e5daf87ed9c5
swapnildey <swapnildey94@gmail.com>
2019-01-13 21:31:40 +0530

    added section attempt bar chart to performance page

---
+++ src/Common/Visualization/BarChart.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Label, ResponsiveContainer} from 'recharts';
+
+
+function StackedBarChart(props) {
+
+
+    return (
+        <ResponsiveContainer width={500} height={300}>
+            <BarChart data={props.data}
+                      margin={{top: 30, right: 20, left: 10, bottom: 35}}
+                      padding={{ left: 30, right: 20 }}
+                      layout="vertical">
+                <CartesianGrid strokeDasharray="3 3"/>
+
+                {/*<XAxis dataKey="name" textAnchor="end" tick={{angle: 45}}>
+                    <Label value="Sections" offset={35} position="bottom" />
+                </XAxis>
+                <YAxis/>*/}
+
+                <XAxis type="number">
+                    <Label value={props.title} offset={35} position="bottom" />
+                </XAxis>
+                <YAxis type="category" dataKey="name" textAnchor="end" tick={{fontSize: 12}} />
+
+                <Tooltip/>
+                <Legend align="center" />
+
+                {/*Data Points for the Visualization*/}
+                {
+                    Object.keys(props.data[0]).map((key => {
+                        if (key !== 'name') {
+                            return(
+                                <Bar dataKey={key} stackId="a" fill={props.colors[key]} />
+                            )
+                        }
+                    }))
+                }
+
+            </BarChart>
+        </ResponsiveContainer>
+    )
+
+}
+
+
+export {StackedBarChart}
+
+StackedBarChart.propTypes = {
+
+    /** chart data that needs to be plotted */
+    data: PropTypes.object,
+
+    /** title of the chart that needs to be displayed */
+    title: PropTypes.object,
+
+    /** colors for the data point */
+    colors: PropTypes.object
+
+};
