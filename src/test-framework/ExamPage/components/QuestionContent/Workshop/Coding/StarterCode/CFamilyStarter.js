51177be741f5de484352f66a8d47e316c7a9ef44
swapnildey <swapnildey94@gmail.com>
2019-01-28 22:24:58 +0530

    fixed failing starter code

--- src/test-framework/ExamPage/components/QuestionContent/Workshop/Coding/StarterCode/CFamilyStarter.js
+++ src/test-framework/ExamPage/components/QuestionContent/Workshop/Coding/StarterCode/CFamilyStarter.js
@@ -2,98 +2,86 @@ const CFamilyStarter = [
     {
         "id": 4,
         "name": "C (gcc 7.2.0)",
-        "default": '#include <cmath>\n' +
-        '#include <cstdio>\n' +
-        '#include <vector>\n' +
-        '#include <iostream>\n' +
-        '#include <algorithm>\n' +
-        'using namespace std;\n' +
-        '\n' +
+        "default": '#include <stdio.h>\n' +
+        '#include <string.h>\n' +
+        '#include <math.h>\n' +
+        '#include <stdlib.h>\n' +
         '\n' +
         'int main() {\n' +
-        '    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   \n' +
+        '\n' +
+        '    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    \n' +
         '    return 0;\n' +
-        '}\n'
+        '}'
     },
     {
         "id": 5,
         "name": "C (gcc 6.4.0)",
-        "default": '#include <cmath>\n' +
-        '#include <cstdio>\n' +
-        '#include <vector>\n' +
-        '#include <iostream>\n' +
-        '#include <algorithm>\n' +
-        'using namespace std;\n' +
-        '\n' +
+        "default": '#include <stdio.h>\n' +
+        '#include <string.h>\n' +
+        '#include <math.h>\n' +
+        '#include <stdlib.h>\n' +
         '\n' +
         'int main() {\n' +
-        '    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   \n' +
+        '\n' +
+        '    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    \n' +
         '    return 0;\n' +
-        '}\n'
+        '}'
     },
     {
         "id": 6,
         "name": "C (gcc 6.3.0)",
-        "default": '#include <cmath>\n' +
-        '#include <cstdio>\n' +
-        '#include <vector>\n' +
-        '#include <iostream>\n' +
-        '#include <algorithm>\n' +
-        'using namespace std;\n' +
-        '\n' +
+        "default": '#include <stdio.h>\n' +
+        '#include <string.h>\n' +
+        '#include <math.h>\n' +
+        '#include <stdlib.h>\n' +
         '\n' +
         'int main() {\n' +
-        '    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   \n' +
+        '\n' +
+        '    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    \n' +
         '    return 0;\n' +
-        '}\n'
+        '}'
     },
     {
         "id": 7,
         "name": "C (gcc 5.4.0)",
-        "default": '#include <cmath>\n' +
-        '#include <cstdio>\n' +
-        '#include <vector>\n' +
-        '#include <iostream>\n' +
-        '#include <algorithm>\n' +
-        'using namespace std;\n' +
-        '\n' +
+        "default": '#include <stdio.h>\n' +
+        '#include <string.h>\n' +
+        '#include <math.h>\n' +
+        '#include <stdlib.h>\n' +
         '\n' +
         'int main() {\n' +
-        '    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   \n' +
+        '\n' +
+        '    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    \n' +
         '    return 0;\n' +
-        '}\n'
+        '}'
     },
     {
         "id": 8,
         "name": "C (gcc 4.9.4)",
-        "default": '#include <cmath>\n' +
-        '#include <cstdio>\n' +
-        '#include <vector>\n' +
-        '#include <iostream>\n' +
-        '#include <algorithm>\n' +
-        'using namespace std;\n' +
-        '\n' +
+        "default": '#include <stdio.h>\n' +
+        '#include <string.h>\n' +
+        '#include <math.h>\n' +
+        '#include <stdlib.h>\n' +
         '\n' +
         'int main() {\n' +
-        '    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   \n' +
+        '\n' +
+        '    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    \n' +
         '    return 0;\n' +
-        '}\n'
+        '}'
     },
     {
         "id": 9,
         "name": "C (gcc 4.8.5)",
-        "default": '#include <cmath>\n' +
-        '#include <cstdio>\n' +
-        '#include <vector>\n' +
-        '#include <iostream>\n' +
-        '#include <algorithm>\n' +
-        'using namespace std;\n' +
-        '\n' +
+        "default": '#include <stdio.h>\n' +
+        '#include <string.h>\n' +
+        '#include <math.h>\n' +
+        '#include <stdlib.h>\n' +
         '\n' +
         'int main() {\n' +
-        '    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   \n' +
+        '\n' +
+        '    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    \n' +
         '    return 0;\n' +
-        '}\n'
+        '}'
     },
     {
         "id": 10,
