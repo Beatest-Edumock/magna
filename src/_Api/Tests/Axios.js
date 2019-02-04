780098b69f318f0daa2331a0c8d0b7e71f8f53ab
harshvardhangupta <harshvardhan_gupta@yahoo.com>
2019-02-05 03:51:23 +0530

    Updated axios to use headers instead of cookies.

--- src/_Api/Tests/Axios.js
+++ src/_Api/Tests/Axios.js
@@ -6,6 +6,48 @@ const testFramAxios = axios.create({});
 
 let errCount = 0;
 //
+
+
+let header = null;
+
+testFramAxios.interceptors.request.use((config) => {
+
+    config.headers['ping'] = header;
+
+    return config;
+
+
+}, (error) => {
+    return Promise.reject(error);
+});
+
+
+// just set both, axios as well as testFramAxios to
+// Update local variable (which will be used by pinger)
+axios.interceptors.response.use((response) => {
+
+    if (response.headers.ping) {
+
+        header = response.headers.ping;
+
+    }
+
+    return response;
+});
+
+
+testFramAxios.interceptors.response.use((response) => {
+
+    if (response.headers.ping) {
+
+        header = response.headers.ping;
+
+    }
+
+    return response;
+});
+
+
 testFramAxios.interceptors.response.use(null,
     function (error) {
 
