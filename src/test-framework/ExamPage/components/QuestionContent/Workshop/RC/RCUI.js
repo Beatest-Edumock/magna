79416fd33deaec922bd9e6e52dfab5b0e23d53fc
harshvardhangupta <harshvardhan_gupta@yahoo.com>
2019-04-23 16:35:46 +0530

    Increased width of rc passage

--- src/test-framework/ExamPage/components/QuestionContent/Workshop/RC/RCUI.js
+++ src/test-framework/ExamPage/components/QuestionContent/Workshop/RC/RCUI.js
@@ -5,11 +5,11 @@ function RCUI(props) {
     return(
         <div className="container-fluid ">
         <div className="row ">
-            <div className="col-md-6 mx-md-1 my-sm-3 card h-60vh-md-scroll scroll-x-auto">
+            <div className="col-md-8  my-sm-3 card h-60vh-md-scroll scroll-x-auto">
                 <div dangerouslySetInnerHTML={{__html: props.passageHtml}}/>
             </div>
             <hr/>
-            <div className="col-md-5 my-sm-3 m-sm-0 py-sm-2 border h-60vh-md-scroll">
+            <div className="col-md-4 my-sm-3 m-sm-0 py-sm-2 border h-60vh-md-scroll">
                 <MCQ question={props.question}/>
             </div>
         </div>
