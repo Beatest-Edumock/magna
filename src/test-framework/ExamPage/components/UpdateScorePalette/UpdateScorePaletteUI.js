d81650e41a71b9dce37383f80a67da848e814e27
harshvardhangupta <harshvardhan_gupta@yahoo.com>
2019-04-28 13:27:18 +0530

    Removed question level time spent

--- src/test-framework/ExamPage/components/UpdateScorePalette/UpdateScorePaletteUI.js
+++ src/test-framework/ExamPage/components/UpdateScorePalette/UpdateScorePaletteUI.js
@@ -8,7 +8,7 @@ function UpdateScorePaletteUI(props) {
         <div>
             {toDisplay &&
             <div className="my-2">
-                <div className="alert-success p-2 rounded">Time spent on question <br/>{props.timeSpent} seconds</div>
+                {/*<div className="alert-success p-2 rounded">Time spent on question <br/>{props.timeSpent} seconds</div>*/}
                 <div><kbd>Marks Allotted : {props.maxScore}</kbd></div>
                 <div><kbd>Marks Secured : {props.obtainedScore}</kbd></div>
                 <div className="my-2">
